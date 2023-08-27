4656721a36dff37de23fdbeb8117aef7b61109ff
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-08-27 23:48:01 +0700

    fill out most of the form

--- src/components/form/landStreet/index.tsx
+++ src/components/form/landStreet/index.tsx
@@ -1,9 +1,60 @@
 import styles from "../landCity/styles.module.css";
 import {Input, Select, Space} from "antd";
 import {useEffect, useState} from "react";
+import FirstLandForm from "../../../hook/builder";
 
 export default function LandStreet() {
     const [widthOneSix, setOneSix] = useState(0)
+    const regexNum = /^[0-9]+$/;
+
+    function testNumString(textString: string | null) {
+        if (!textString) return false;
+        return regexNum.test(textString)
+    }
+
+    function getNumAllString(textString: string | null) {
+        if (!textString) return 0
+        if (regexNum.test(textString)) {
+            return parseInt(textString)
+        }
+        return 0;
+    }
+    const [currentForm, setForm] = useState({
+        sothua: '',
+        soToBanDo: '',
+        hoSoPhapLyCity: localStorage.getItem('hoSoPhapLyCity'),
+        thucTeCity: localStorage.getItem('thucTeCity'),
+        hoSoPhapLyProvince: localStorage.getItem('hoSoPhapLyProvince'),
+        thucTeProvince: localStorage.getItem('thucTeProvince'),
+
+        hoSoPhapLyStreet: localStorage.getItem('hoSoPhapLyStreet') ? localStorage.getItem('hoSoPhapLyStreet') : '',
+        thucTeStreet: localStorage.getItem('thucTeStreet') ? localStorage.getItem('thucTeStreet') : '',
+        hoSoPhapLyDetail: localStorage.getItem('hoSoPhapLyDetail') ? localStorage.getItem('hoSoPhapLyDetail') : '',
+        thucTeDetail: localStorage.getItem('thucTeDetail') ? localStorage.getItem('thucTeDetail') : '',
+        hoSoPhapLyTown: localStorage.getItem('hoSoPhapLyTown') ? localStorage.getItem('hoSoPhapLyTown') : '',
+        thucTeTown: localStorage.getItem('thucTeTown') ? localStorage.getItem('thucTeTown') : '',
+        duongKhungGia: testNumString(localStorage.getItem('duongKhungGia')) ? getNumAllString(localStorage.getItem('duongKhungGia')) : 0,
+        khoangCachDuongChinh: testNumString(localStorage.getItem('khoangCachDuongChinh')) ? getNumAllString(localStorage.getItem('khoangCachDuongChinh')) : 0,
+        loaiDuongTiepGiap: localStorage.getItem('loaiDuongTiepGiap') ? localStorage.getItem('loaiDuongTiepGiap') : '',
+        khoangCachSTB: testNumString(localStorage.getItem('khoangCachSTB')) ? getNumAllString(localStorage.getItem('khoangCachSTB')) : 0,
+        viTri: localStorage.getItem('viTri') ? localStorage.getItem('viTri') : '',
+        viTriKhungGia: testNumString(localStorage.getItem('viTriKhungGia')) ? getNumAllString(localStorage.getItem('viTriKhungGia')) : 0,
+        doRongDuong1: testNumString(localStorage.getItem('doRongDuong1')) ? getNumAllString(localStorage.getItem('doRongDuong1')) : 0,
+        doRongDuong2: testNumString(localStorage.getItem('doRongDuong2')) ? getNumAllString(localStorage.getItem('doRongDuong2')) : 0,
+        khuVuc: localStorage.getItem('khuVuc') ? localStorage.getItem('khuVuc') : '',
+        ghiChu: localStorage.getItem('ghiChu') ? localStorage.getItem('ghiChu') : '',
+
+        huongChinh: localStorage.getItem('huongChinh') ? localStorage.getItem('huongChinh') : '',
+        hinhDang: localStorage.getItem('hinhDang') ? localStorage.getItem('hinhDang') : '',
+        soMatTien: localStorage.getItem('soMatTien') ? localStorage.getItem('soMatTien') : '',
+        kichThuocMatTien: testNumString(localStorage.getItem('kichThuocMatTien')) ? getNumAllString(localStorage.getItem('kichThuocMatTien')) : 0,
+        kichThuocChieuDai: testNumString(localStorage.getItem('kichThuocChieuDai')) ? getNumAllString(localStorage.getItem('kichThuocChieuDai')) : 0,
+        dienTichKhuonVien: testNumString(localStorage.getItem('dienTichKhuonVien')) ? getNumAllString(localStorage.getItem('dienTichKhuonVien')) : 0,
+        dienTichPhuHopQuyHoach: testNumString(localStorage.getItem('dienTichPhuHopQuyHoach')) ? getNumAllString(localStorage.getItem('dienTichPhuHopQuyHoach')) : 0,
+        dienTichKhongPhuHopQuyHoach: testNumString(localStorage.getItem('dienTichKhongPhuHopQuyHoach')) ? getNumAllString(localStorage.getItem('dienTichKhongPhuHopQuyHoach')) : 0,
+        dienTichSuDungRieng: testNumString(localStorage.getItem('dienTichSuDungRieng')) ? getNumAllString(localStorage.getItem('dienTichSuDungRieng')) : 0,
+        dienTichSuDungChung: testNumString(localStorage.getItem('dienTichSuDungChung')) ? getNumAllString(localStorage.getItem('dienTichSuDungChung')) : 0,
+    })
 
     function editSize() {
         const landCity: HTMLElement | null = document.querySelector("#landCity")
@@ -67,28 +118,94 @@ export default function LandStreet() {
             </thead>
             <tr className={styles.tableThree}>
                 <td>Diện tích khuôn viên (m2)</td>
-                <td><Input placeholder="STB nhập và cho sửa"/></td>
-                <td><Input placeholder="Hệ thống tự nhập và cho sửa"/></td>
+                <td><Input placeholder="STB nhập và cho sửa"
+                           value={currentForm.dienTichKhuonVien}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichKhuonVien: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichKhuonVien(getNumAllString(event.target.value))
+                           }}
+                /></td>
+                <td><Input placeholder="Hệ thống tự nhập và cho sửa"
+                           value={currentForm.dienTichKhuonVien}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichKhuonVien: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichKhuonVien(getNumAllString(event.target.value))
+                           }}
+                /></td>
             </tr>
             <tr className={styles.tableThree}>
                 <td>Diện tích phù hợp quy hoạch (m2)</td>
-                <td><Input placeholder="STB nhập và cho sửa"/></td>
-                <td><Input placeholder="Hệ thống tự nhập và cho sửa"/></td>
+                <td><Input placeholder="STB nhập và cho sửa"
+                           value={currentForm.dienTichPhuHopQuyHoach}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichPhuHopQuyHoach: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichPhuHopQuyHoach(getNumAllString(event.target.value))
+                           }}
+                /></td>
+                <td><Input placeholder="Hệ thống tự nhập và cho sửa"
+                           value={currentForm.dienTichPhuHopQuyHoach}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichPhuHopQuyHoach: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichPhuHopQuyHoach(getNumAllString(event.target.value))
+                           }}
+                /></td>
             </tr>
             <tr className={styles.tableThree}>
                 <td>Diện tích không phù hợp quy hoạch (m2)</td>
-                <td><Input placeholder="STB nhập và cho sửa"/></td>
-                <td><Input placeholder="Hệ thống tự nhập và cho sửa"/></td>
+                <td><Input placeholder="STB nhập và cho sửa"
+                           value={currentForm.dienTichKhongPhuHopQuyHoach}
+                           onChange={(event) => {
+                               setForm({
+                                   ...currentForm,
+                                   dienTichKhongPhuHopQuyHoach: getNumAllString(event.target.value)
+                               })
+                               FirstLandForm.set_dienTichKhongPhuHopQuyHoach(getNumAllString(event.target.value))
+                           }}
+                /></td>
+                <td><Input placeholder="Hệ thống tự nhập và cho sửa"
+                           value={currentForm.dienTichKhongPhuHopQuyHoach}
+                           onChange={(event) => {
+                               setForm({
+                                   ...currentForm,
+                                   dienTichKhongPhuHopQuyHoach: getNumAllString(event.target.value)
+                               })
+                               FirstLandForm.set_dienTichKhongPhuHopQuyHoach(getNumAllString(event.target.value))
+                           }}
+                /></td>
             </tr>
             <tr className={styles.tableThree}>
                 <td>Diện tích sử dụng riêng (m2)</td>
-                <td><Input placeholder="STB nhập và cho sửa"/></td>
-                <td><Input placeholder="Hệ thống tự nhập và cho sửa"/></td>
+                <td><Input placeholder="STB nhập và cho sửa"
+                           value={currentForm.dienTichSuDungRieng}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichSuDungRieng: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichSuDungRieng(getNumAllString(event.target.value))
+                           }}
+                /></td>
+                <td><Input placeholder="Hệ thống tự nhập và cho sửa"
+                           value={currentForm.dienTichSuDungRieng}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichSuDungRieng: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichSuDungRieng(getNumAllString(event.target.value))
+                           }}
+                /></td>
             </tr>
             <tr className={styles.tableThree}>
                 <td>Diện tích sử dụng chung (m2)</td>
-                <td><Input placeholder="STB nhập và cho sửa"/></td>
-                <td><Input placeholder="Hệ thống tự nhập và cho sửa"/></td>
+                <td><Input placeholder="STB nhập và cho sửa"
+                           value={currentForm.dienTichSuDungChung}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichSuDungChung: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichSuDungChung(getNumAllString(event.target.value))
+                           }}
+                /></td>
+                <td><Input placeholder="Hệ thống tự nhập và cho sửa"
+                           value={currentForm.dienTichSuDungChung}
+                           onChange={(event) => {
+                               setForm({...currentForm, dienTichSuDungChung: getNumAllString(event.target.value)})
+                               FirstLandForm.set_dienTichSuDungChung(getNumAllString(event.target.value))
+                           }}
+                /></td>
             </tr>
         </table>
     </div>
